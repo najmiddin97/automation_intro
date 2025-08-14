2817f88efb850a34b05deb9d1e9210908c25f13f
najmiddin97 <najmiddinzulfiddinovich@gmail.com>
2025-08-14 10:39:33 +0500

    Update 11test_api.py

--- 11test_api.py
+++ 11test_api.py
@@ -1,5 +1,5 @@
 import requests
 
 
-resrponse = requests.get("https://api-inextlynk.upgrow.uz/api/v1/marketplace/home-page-product-categories/?page=1")
+resrponse = requests.get("https://inextlynk.upgrow.uz/api/v1/marketplace/home-page-product-categories/?page=1")
 print(resrponse)
